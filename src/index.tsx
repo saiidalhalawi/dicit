7adf5f762a8aac1df38230d1bad6bf70c9a917a9
saiidalhalawi <saiid.kk@gmail.com>
2018-09-17 01:44:38 +0900

    Store undisplayed quotes to localStrage.

--- src/index.tsx
+++ src/index.tsx
@@ -3,10 +3,21 @@ import * as ReactDOM from 'react-dom';
 
 import { QuoteFrame } from './components/QuoteFrame';
 
-const quotesCollection = require('./../data/quotes.json');
+let remainedQuotes = [];
+if ('remainedQuotes' in localStorage) {
+  remainedQuotes = JSON.parse(localStorage.getItem('remainedQuotes'));
+}
 
-const index = Math.floor(Math.random() * quotesCollection.quotes.length);
-const initialQuote = quotesCollection.quotes[index];
+if (remainedQuotes.length === 0) {
+  const wholeQuotes = require('./../data/quotes.json');
+  remainedQuotes = wholeQuotes.quotes;
+}
+
+const index = Math.floor(Math.random() * remainedQuotes.length);
+const initialQuote = remainedQuotes[index];
+
+remainedQuotes.splice(index, 1);
+localStorage.setItem('remainedQuotes', JSON.stringify(remainedQuotes));
 
 ReactDOM.render(
   <QuoteFrame quote={initialQuote} />,
