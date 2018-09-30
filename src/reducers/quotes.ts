6b8c3f28a2c372908c530a354b2d04006ecc8eb7
saiidalhalawi <saiid.kk@gmail.com>
2018-10-01 01:39:55 +0900

    Add an action to fetch dynamically

--- src/reducers/quotes.ts
+++ src/reducers/quotes.ts
@@ -7,13 +7,30 @@ export interface DicitState {
   quote: Quote;
 }
 
-const initialState: DicitState = { quote: { sentence: '', author: '' } };
+const fetchNewQuote = (): Quote => {
+  let remainedQuotes = [];
+  if ('remainedQuotes' in localStorage) {
+    remainedQuotes = JSON.parse(localStorage.getItem('remainedQuotes'));
+  }
+
+  if (remainedQuotes.length === 0) {
+    const wholeQuotes = require('./../../data/quotes.json');
+    remainedQuotes = wholeQuotes.quotes;
+  }
+
+  const index = Math.floor(Math.random() * remainedQuotes.length);
+  remainedQuotes.splice(index, 1);
+  localStorage.setItem('remainedQuotes', JSON.stringify(remainedQuotes));
+
+  return remainedQuotes[index];
+};
+
+const initialState: DicitState = { quote: fetchNewQuote() };
 
 export const quotesReducer = reducerWithInitialState(initialState)
   .case(dicitActions.init, (state) => {
     return Object.assign({}, state, { initialState });
   })
   .case(dicitActions.fetchQuote, (state) => {
-    const currQuote = { sentence: 'The sinews of war are not gold, but good soldiers.', author: 'Niccoló Machiavelli' };
-    return Object.assign({}, state, { currQuote });
+    return Object.assign({}, state, { initialState });
   });
