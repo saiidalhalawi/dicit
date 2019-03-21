923b3a3dbb3325bc19f8ab25b477ebdb1124e1c9
saiidalhalawi <saiid.kk@gmail.com>
2019-03-21 15:44:03 +0900

    Fix styles

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,22 @@
 import * as React from 'react';
+import styled from 'styled-components';
+
 import QuoteFrameContainer from './containers/quoteFrameContainer';
 import QuoteStats from './containers/quoteStatsContainer';
+
+const Wrapper = styled.div`
+  position: relative;
+  width: 100%;
+  height: 100%;
+`;
 
 class App extends React.Component {
   render() {
     return (
-      <div>
+      <Wrapper>
         <QuoteFrameContainer />
         <QuoteStats />
-      </div>
+      </Wrapper>
     );
   }
 }
--- src/components/QuoteStatsComponent.tsx
+++ src/components/QuoteStatsComponent.tsx
@@ -5,10 +5,24 @@ import { QuoteStatsState } from '../reducers/stats';
 
 type StatsProps = QuoteStatsState;
 
+const QuoteStatsFrame = styled.div`
+  position: fixed;
+  bottom: 0;
+  right: 0;
+  margin: auto;
+  padding: 1rem;
+  text-align: right;
+`;
+
+const QuoteStatsContents = styled.span`
+  display: inline-block;
+  padding: 1rem 0;
+`;
+
 export const QuoteStatsComponent = (props: StatsProps) => {
   return (
-    <div>
-      <span>{props.stats.consumed} / {props.stats.whole}</span>
-    </div>
+    <QuoteStatsFrame>
+      <QuoteStatsContents>{props.stats.consumed} / {props.stats.whole}</QuoteStatsContents>
+    </QuoteStatsFrame>
   );
 }
